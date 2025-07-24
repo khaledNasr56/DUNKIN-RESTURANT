bd46d20702b95804aabe9b80f3ffa1872f3200a0
khaledNasr56 <kn3846228@gmail.com>
2025-07-24 04:37:19 +0300

    Fix vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite';
 
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins:[tailwindcss(), react()]
-  ,
-});
+  base: '/dunkin-resturant-r12h/', // ✨ اسم الريبو هنا بالظبط
+  plugins: [tailwindcss(),react()],
+})
